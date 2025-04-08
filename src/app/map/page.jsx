16ebd5137e41fc9cf76091445ce25f962f024e20
jorgebaptista <jorgebaptista.dev@outlook.com>
2025-04-08 16:37:00 +0100

    feat: add simple navbar to map page

--- src/app/map/page.jsx
+++ src/app/map/page.jsx
@@ -1,10 +1,12 @@
+import SimpleNavBar from "@/components/layout/SimpleNavBar";
 import MapClient from "@/components/MapClient";
 
 export default function MapPage() {
     return (
         <div className="w-full h-screen flex flex-col">
+            <SimpleNavBar />
             <div className="p-4 bg-white shadow">
-                <h1 className="text-2xl font-semibold text-green-700">Mapa de Deflorestação em Portugal</h1>
+                <h1 className="text-2xl font-semibold text-green-700">Mapa de Deforestação em Portugal</h1>
             </div>
             <div className="flex-grow">
                 <MapClient />
